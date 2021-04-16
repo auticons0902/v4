2fae5ecc33f6ba16119e40b5631e354b2d76c3e3
ProjectSpotCheck <nguyen.k.nhan0902@gmail.com>
2021-04-15 21:11:54 -0700

    Modifed Contact message
    - to show that Nhi's looking for full-time opportunities in Finance and BA.

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -55,8 +55,9 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I'm not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I'll try my best to get back to you!
+        I'm currently looking for full-time opportunities in Finance and Business Analytics. Whether
+        you would like to contact regarding these opportunities or just want to say hi, my inbox is
+        always open. I'll make sure to get back to you as soon as I can!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
